bf19a5242206b1d34f203aedd01a0d2ce7ceb709
Sean B <butts.sean01@gmail.com>
2020-08-03 21:23:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  original = array.sort
+  reversed = original.reverse
+  reversed
+end
+
+def sort_array_char_count(array)
+  arrary = array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array.insert(1,array.delete_at(2))
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+    array = array.each
+  array.join
+end
+
+def add_s(array)
+  array.split.collect{|x| x = array.join("s")}
+  
+end
